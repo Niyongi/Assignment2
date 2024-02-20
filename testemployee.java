e5527a85ad2730baa577e5ba5bb60e77015fcc5d
Niyongi <159782070+Niyongi@users.noreply.github.com>
2024-02-20 01:35:41 -0800

    Add files via upload

---
+++ testemployee.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+class employee{
+    
+    int id;
+    String name;
+    float salary;
+    void employee(int i,String n,float s){
+        id=i;
+        name=n;
+        salary=s;
+    }
+    
+    void display(){
+        System.out.println(id+""+name+""+salary);
+    }
+}
+public class testemployee {
+    public static void main(String[] args) {
+        employee s1=new     
+    }
+    
+}
